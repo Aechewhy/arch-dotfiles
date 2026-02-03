b8bd33fc956d146c8f0fd3c192187635e4851a6a
Aechewhy <harryhuy2472004@gmail.com>
2026-02-03 11:27:50 +0700

    add obs studio

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -9,7 +9,7 @@ alias editz="nvim ~/.zshrc"
 alias git.="lazygit -p /home/aechewhy/.dotfiles"
 
 alias l="eza"
-alias ls='eza --long --icons --classify'
-alias lt="eza --long --header --icons --git --git-repos -B --total-size -U -u -m --time-style=long-iso"
+alias le='eza --long --icons --classify -a'
+alias lt="eza --long --header -a --icons --git --git-repos -B --total-size -U -u -m --time-style=long-iso"
 
 alias ns='nvim -c "lua require(\"persistence\").load({ last = true })"'
