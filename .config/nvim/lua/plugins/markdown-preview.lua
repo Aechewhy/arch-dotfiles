58b271f9e1a9699200ba1bc3afeffecb704f6e98
Aechewhy <harryhuy2472004@gmail.com>
2026-03-16 16:53:13 +0700

    Auto backup: 2026-03-16 16:53:13

--- .config/nvim/lua/plugins/markdown-preview.lua
+++ .config/nvim/lua/plugins/markdown-preview.lua
@@ -2,7 +2,15 @@ return {
   "iamcco/markdown-preview.nvim",
   cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
   ft = { "markdown" },
-  build = function()
-    vim.fn["mkdp#util#install"]()
+  -- Using the shell script directly avoids the "Unknown function" error
+  build = "sh -c 'cd app && ./install.sh'",
+  init = function()
+    vim.g.mkdp_filetypes = { "markdown" }
+    vim.g.mkdp_preview_options = {
+      -- This is the "Obsidian" switch:
+      mkit = {
+        breaks = true, -- This turns single line breaks into <br>
+      },
+    }
   end,
 }
